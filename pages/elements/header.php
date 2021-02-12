606015ddc4c5b34b11ef8ba74ae68943156ecdf3
Abrellr <69308389+Abrellr@users.noreply.github.com>
2021-02-12 19:09:53 +0100

    iron style

--- pages/elements/header.php
+++ pages/elements/header.php
@@ -1,50 +1,42 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" content="">
-    <meta name="author" content="">
 
-    <title>Startseite</title>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <title>Startseite</title>
+  <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+  <!-- Optional theme -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <link rel="stylesheet" href="../style/style.css">
 
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+</head>
 
-    <!-- Optional theme -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-   <!--[if lt IE 9]>
-     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-   <![endif]-->
-  </head>
-
-  <body>
-
-    <nav class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="index.php">Testing PHP & MySQL</a>
-        </div>
-        <div id="navbar" class="collapse navbar-collapse">
-          <ul id="navItem" class="nav navbar-nav">
-            <li class="active"><a href="index.php">Home</a></li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-
-    <br /><br />
-
+<body>
+  <nav class="navbar navbar-inverse navbar-fixed-top">
     <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="index.php">Testing PHP & MySQL</a>
+      </div>
+      <div id="navbar" class="collapse navbar-collapse">
+        <ul id="navItem" class="nav navbar-nav">
+          <li class="active"><a href="index.php"><i class="bi bi-house-door-fill"></i><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="white" class="bi bi-house-door-fill" viewBox="0 0 16 16">
+                <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5z" />
+              </svg></a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <br /><br />
--- pages/index.php
+++ pages/index.php
@@ -1,99 +1,95 @@
 <?php include("../database.php"); ?>
 <?php include("./elements/header.php"); ?>
 
-
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" content="">
-    <meta name="author" content="">
 
-    <title>Startseite</title>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+  <meta name="description" content="">
+  <meta name="author" content="">
 
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+  <title>Startseite</title>
 
-    <!-- Optional theme -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
-    <link rel="stylesheet" href="./style/style.css">
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-   <!--[if lt IE 9]>
-     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-   <![endif]-->
-  </head>
+  <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
-  <body>
+  <!-- Optional theme -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+  <link rel="stylesheet" href="./style/style.css">
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+</head>
+
+
+<div class="container">
+  <h1 id="blogTitle">Startseite des Blogs</h1>
+  <p class="lead">This blog is for testing MySQL & PHP on Xampp</p>
+  <!--fetch and loop the data from database here-->
+  <?php $res = fetch_posts(); ?>
+  <ul>
+    <?php foreach ($res as $row) : ?>
+      <li>
+        <a href="post.php?id=<?php echo $row['id']; ?>">
+          <?php echo $row["title"]; ?>
+        </a>
+      </li>
+      <p>
+        <?php echo $row["content"]; ?>
+      </p>
+    <?php endforeach; ?>
+  </ul>
+  </br>
+
+  <!--This is the card section -->
+  <section class="blog-area pt-5 pb-5">
     <div class="container">
-      <h1>Startseite des Blogs</h1>
-      <p class="lead">This blog is for testing MySQL & PHP on Xampp</p>
-      <!--fetch and loop the data from database here-->
-      <?php $res = fetch_posts();?>
-      <ul>
-        <?php foreach ($res AS $row): ?>
-          <li>
-          <a href="post.php?id=<?php echo $row['id']; ?>">
-            <?php echo $row["title"]; ?>
-            </a>
-          </li>
-          <p>
-            <?php echo $row["content"]; ?>
-          </p>
-        <?php endforeach; ?>
-      </ul>
-      </br>
+      <div class="row">
+        <div class="col-xl-12 col-sm-12 col-xs-12 text-center">
+          <div class="section-title">
+            <h2>Latest Blog Posts</h2>
+            <p>There are many variations of passages of Lorem Ipsum available, but the blog post cards below are rendered using foreach loop.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="row" style="padding: 2em;">
+        <?php $res = fetch_posts(); ?>
+        <?php foreach ($res as $row) : ?>
 
-<!--This is the card section -->
-      <section class="blog-area pt-5 pb-5">
-            <div class="container">
-                <div class="row">
-                    <div class="col-xl-12 text-center pb-3">
-                        <div class="section-title service-title">
-                            <h2>Latest Blog Posts</h2>
-                            <p>There are many variations of passages of Lorem Ipsum available, but the blog post cards below are rendered using foreach loop.
-                              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <?php $res = fetch_posts();?>
-                <?php foreach ($res AS $row): ?>
-                <div class="row">
-                    <div class="col-xl-4 col-lg-4 col-md-6 mb-30">
-                        <div class="blog-wrapper home-blog-wrapper white-bg">
-                            <div class="blog-thumb">
-                                <a href="post.php?id=<?php echo $row['id']; ?>" >
-                                    <img src="https://images.unsplash.com/photo-1529380996459-a5a4eeea810a?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=750&q=80" alt="" />
-                                </a>
-                            </div>
-                            <div class="meta-info">
-                                <ul>
-                                    <li class="posts-time">January 08, 2019</li>
-                                </ul>
-                            </div>
-                            <div class="blog-content home-blog">
-                                <h2 class="blog-title">
-                                <?php echo $row["title"]; ?>
-                                </h2>
-                                <p>
-                                <?php echo $row["content"]; ?>
-                                </p>
-                            </div>
-                            <div class="link-box home-blog-link">
-                              <a href="post.php?id=<?php echo $row['id']; ?>" >Read More</a>
-                            </div>
-                        </div>
-                    </div>
-                    <?php endforeach; ?>
-                </div>
+          <div id="cardCol" class="col-xl-4 col-lg-4 col-md-6 col-sm-12" style="margin-bottom: 2em;">
+            <div class="blog-wrapper home-blog-wrapper white-bg">
+              <div class="blog-thumb">
+                <a href="post.php?id=<?php echo $row['id']; ?>">
+                  <img src="https://images.unsplash.com/photo-1529380996459-a5a4eeea810a?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=750&q=80" alt="" />
+                </a>
+              </div>
+              <div class="meta-info">
+                <ul>
+                  <li class="posts-time">January 08, 2019</li>
+                </ul>
+              </div>
+              <div class="blog-content home-blog">
+                <h2 class="blog-title">
+                  <?php echo $row["title"]; ?>
+                </h2>
+                <p>
+                  <?php echo $row["content"]; ?>
+                </p>
+              </div>
+              <div class="link-box home-blog-link">
+                <a href="post.php?id=<?php echo $row['id']; ?>">Read More</a>
+              </div>
             </div>
-        </section>
-
+          </div>
+        <?php endforeach; ?>
+      </div>
     </div>
+  </section>
+</div>
 
+</body>
 <?php include("./elements/footer.php"); ?>
